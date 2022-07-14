352dd3d6b9e7700be145d2398017f70ebf86452c
Tessa Grainger <tessa.robyn@outlook.com>
2022-07-14 15:13:13 +0100

    Created basic grid structure

--- gameScript.js
+++ gameScript.js
@@ -1,7 +1,12 @@
 function addGrid () {
+    const squareSize=30;
     const gridContainer=document.getElementById("grid");
     const width=gridContainer.offsetWidth;
     const height=gridContainer.offsetHeight;
+    const c = Math.floor(width/squareSize);
+    const r = Math.floor(height/squareSize);
+    gridContainer.style.gridTemplateColumns="repeat("+c+", 1fr)";
+    gridContainer.style.gridTemplateRows="repeat("+r+", 1fr)";
 }
 
 addGrid();
