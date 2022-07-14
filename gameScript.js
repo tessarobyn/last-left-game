9955b04a98802db84ce90001a3f4fccfaafb3768
Tessa Grainger <tessa.robyn@outlook.com>
2022-07-14 14:24:37 +0100

    Created basic setup for game screen

---
+++ gameScript.js
@@ -0,0 +1,7 @@
+function addGrid () {
+    const gridContainer=document.getElementById("grid");
+    const width=gridContainer.offsetWidth;
+    const height=gridContainer.offsetHeight;
+}
+
+addGrid();
--- script.js
+++ script.js
@@ -3,5 +3,14 @@ function setSquareSize() {
     squareContainer.style.height=(squareContainer.offsetWidth/3)+"px";
 }
 
+function startGame() {
+    window.location.href="game.html";
+}
+
 setSquareSize();
 window.onresize=setSquareSize;
+
+const playButton = document.getElementById("play");
+playButton.addEventListener("click", () => {
+    startGame();
+})
